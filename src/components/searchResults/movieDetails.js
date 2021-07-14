955160372e5c1549e4550468c049798cc8fbee4b
kunal mathur <kunal.mathur07@gmail.com>
2021-07-14 19:18:35 +0530

    Adde rated and ratings to the movie details

--- src/components/searchResults/movieDetails.js
+++ src/components/searchResults/movieDetails.js
@@ -1,9 +1,30 @@
+const IMDB = "Internet Movie Database";
 export function MovieDetails(domNode, props = {}) {
   const { movieDetails } = props;
+  function getRatings(movieDetails) {
+    return movieDetails.Ratings.map((rating) => {
+      rating.Source = rating.Source === IMDB ? "IMDB" : rating.Source;
+      return ` ${rating.Source} ${rating.Value} `;
+    });
+  }
+
   let element = document.getElementById(`${domNode}`);
   element.innerHTML = `<div class="view-movie-details">
-                            <div><strong>Year:</strong> ${movieDetails.Year}</div>
-                            <div> <strong>Actors:</strong> ${movieDetails.Actors}</div>
-                            <div> <strong>Plot:</strong> ${movieDetails.Plot}</div>
+                            <div><strong>Year:</strong> ${
+                              movieDetails.Year
+                            }</div>
+                            <div> <strong> Rated:</strong> ${
+                              movieDetails.Rated
+                            } </div>
+                            <div> <strong> Rating:</strong>${getRatings(
+                              movieDetails
+                            )}</div>
+                            <div> <strong>Actors:</strong> ${
+                              movieDetails.Actors
+                            }</div>
+
+                            <div> <strong>Plot:</strong> ${
+                              movieDetails.Plot
+                            }</div>
                         </div>`;
 }
